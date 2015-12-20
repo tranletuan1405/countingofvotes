ad5b86b53cf0663bfa24c52b38e945b877d6e6e8
tuantran <tranletuan1405@gmail.com>
2015-12-20 17:00:57 +0700

    Restart this project

---
+++ CountingOfVotes/src/main/java/edu/uit/tuoitre/CountingOfVotesApplication.java
@@ -0,0 +1,12 @@
+package edu.uit.tuoitre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountingOfVotesApplication {
+
+    public static void main(String[] args) {
+    	System.out.println("It's ok");
+    }
+}
