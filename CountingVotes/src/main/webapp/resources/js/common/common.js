341ba3df33aa600f32f23aefa5841c7965fe75c0
Trần Lê Tuấn <tranletuan1405@gmail.com>
2016-02-08 01:08:34 +0700

    update projects

--- CountingVotes/src/main/webapp/resources/js/common/common.js
+++ CountingVotes/src/main/webapp/resources/js/common/common.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
 
+	 $('.form-datetime').datetimepicker({
+		format: "dd / mm / yyyy - hh:ii",
+		weekStart : 1,
+		todayBtn : 1,
+		autoclose : 1,
+		todayHighlight : 1,
+		startView : 2,
+		forceParse : 0,
+		showMeridian : 1
+	});
 	
 });
 
